498eebeacda28be3ca248698599f09023461599a
Demothrin <scyth212@hotmail.com>
2025-09-29 20:00:42 -0500

    completed the homework learning activity for week 5

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assign1 = new Assignment("Samuel Bennett", "Multiplication");
+        string asum = assign1.GetSummary();
+        Console.WriteLine(asum);
+
+        Console.WriteLine();
+
+        MathAssignment student1 = new MathAssignment("Roberto Rodriquez", "Fractions", "7.3", "8-19");
+        string sum1 = student1.GetSummary();
+        string list1 = student1.GetHomeworkList();
+        Console.WriteLine($"{sum1}\n{list1}");
+
+        Console.WriteLine();
+
+        WritingAssignment student2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        string sum2 = student2.GetSummary();
+        string list2 = student2.GetWritingInformation();
+        Console.WriteLine($"{sum2}\n{list2}");
     }
 }
