f9633d30b3d4774732451e0fec510483e74a5d63
Demothrin <scyth212@hotmail.com>
2025-10-06 20:01:48 -0500

    Completed learning activity for week 6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square newShape = new Square("red", 5);
+        shapes.Add(newShape);
+
+        Rectangle newRec = new Rectangle("Blue", 3, 4);
+        shapes.Add(newRec);
+
+        Circle newCir = new Circle("Green", 13);
+        shapes.Add(newCir);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The color of the shape is {color} and the area is {area}");
+        }
+      
     }
 }
